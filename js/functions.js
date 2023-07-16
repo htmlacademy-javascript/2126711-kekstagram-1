f15e07677ce4d1c78892b599b6a5e9d603115e23
p1mk1n <evgen@p1mk1n.ru>
2023-07-16 17:21:13 +0300

    Добавляет домашнее задание по второму разделу

---
+++ js/functions.js
@@ -0,0 +1,47 @@
+
+// Номер 1
+function isPalindrom() {
+  for () {
+
+  }
+}
+
+isPalindrom('Довод');
+
+// Номер 2
+
+function getNumber(str, base) {
+  let parsed = parseInt(str, 10);
+  if (isNaN(parsed)) {
+    return NaN;
+  }
+  return parsed;
+}
+
+console.log(getNumber('22 бобра'));
+
+
+// Номер 3
+
+function getAddString() {
+}
+
+getAddString('q', 4, 'we');
+
+
+// Номер 4
+
+function getStringLength(str) {
+  let string = str.length;
+
+  if (string > 15) {
+    alert('Превышено количество символов!')
+    return false;
+  }
+
+  return string;
+}
+
+console.log(getStringLength('22 бобра'));
+console.log(getStringLength('22'));
+console.log(getStringLength('138 общипанных куриц'));
