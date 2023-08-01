06b91206fe33bb2bcb367e4671d1b14bf847ed36
Keks <keks@htmlacademy.ru>
2023-08-01 13:05:11 +0300

    :heavy_check_mark: Сборка #2

--- 2/js/functions.js
+++ 2/js/functions.js
@@ -4,53 +4,60 @@
 
 const isPalindrom = (str) => {
   let string = str.replaceAll(" ").toLowerCase();
-  string.split('').reverse().join('');
+  let reverseString = string.split('').reverse().join('');
 
-  if (str === string) {
+  if (string === reverseString) {
     return true;
   }
 
   return false;
-}
+};
 
-isPalindrom('топот');
-isPalindrom('ДовОд');
-isPalindrom('Кекс');
+console.log(isPalindrom('топот'));
+console.log(isPalindrom('ДовОд'));
+console.log(isPalindrom('Кекс'));
 
 // Номер 2
 
 const getNumber = (str) => {
-  let parsed = parseFloat(str.replace(/[^\d]/g, ''));
+  let parsed = parseInt(str.toString().replace(/[^\d]/g, ''));
 
   if (isNaN(parsed)) {
     return NaN;
   }
 
-  return Math.abs(parsed);
-}
+  return parsed;
+};
 
-getNumber('2023 год');
-getNumber('ECMAScript 2022');
-getNumber('1 кефир, 0.5 батона');
-getNumber('агент 007');
-getNumber('а я томат');
-getNumber(2023);
-getNumber(-1);
-getNumber(1.5);
+console.log(getNumber('2023 год'));
+console.log(getNumber('ECMAScript 2022'));
+console.log(getNumber('1 кефир, 0.5 батона'));
+console.log(getNumber('агент 007'));
+console.log(getNumber('а я томат'));
+console.log(getNumber(2023));
+console.log(getNumber(-1));
+console.log(getNumber(1.5));
 
 // Номер 3
-
-const getAddString = (startSymbols, amountSymbols, additionalSymbols) => {
-  for (let i = startSymbols; i <= amountSymbols;  i += additionalSymbols) {
-
-  }
+const getAddSymbol = (length, directLength, addSymbol) => {
+  let availableLength = directLength - length;
+  return addSymbol.slice(0, availableLength);
 }
 
-getAddString('1', 2, '0');
-getAddString('1', 4, '0');
-getAddString('q', 4, 'werty');
-getAddString('q', 4, 'we');
-getAddString('qwerty', 4, '0');
+const getAddString = (baseString, directLength, addSymbol) => {
+  if (length < directLength) {
+    const newString = getAddSymbol(length, directLength, addSymbol) + baseString;
+    return getAddString(newString, directLength, addSymbol);
+  }
+
+  return baseString;
+};
+
+console.log(getAddString('1', 2, '0'));
+console.log(getAddString('1', 4, '0'));
+console.log(getAddString('q', 4, 'werty'));
+console.log(getAddString('q', 4, 'we'));
+console.log(getAddString('qwerty', 4, '0'));
 
 // Номер 4
 
