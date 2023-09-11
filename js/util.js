1bfa48e1aba721098f9b908a3251ca543ad68fa3
p1mk1n <evgen@p1mk1n.ru>
2023-09-11 22:00:28 +0300

    Добавляет модульность JS-файлов

---
+++ js/util.js
@@ -0,0 +1,11 @@
+const getRandomInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomInteger(0, elements.length - 1)];
+
+export { getRandomInteger, getRandomArrayElement };
