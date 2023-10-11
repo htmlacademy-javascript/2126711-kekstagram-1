a10d575efa76f60a22f86fc0cf5bd2b770a8c769
Keks <keks@htmlacademy.ru>
2023-10-11 13:04:17 +0300

    :heavy_check_mark: Сборка #6

--- 6/js/main.js
+++ 6/js/main.js
@@ -1,4 +1,9 @@
+<<<<<<< HEAD
+import './util.js';
+import './thumbnails.js';
+=======
 import "./create-description.js";
 import "./util.js";
 import "./thumbnails.js";
 
+>>>>>>> a5927f67400b107f4cb0610db05adaa7927fd25d
