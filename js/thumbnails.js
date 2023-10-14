de483d4dc09f9b10e3248ebfb2cfe1b19e4d0cbf
p1mk1n <evgen@p1mk1n.ru>
2023-10-14 19:15:23 +0300

    Исправляет модуль thumbnails

--- js/thumbnails.js
+++ js/thumbnails.js
@@ -2,40 +2,33 @@ import { createDescriptionPhoto } from './create-description.js';
 
 const thumbnailsList = document.querySelector('.pictures');
 const thumbnailsTemplate = document.querySelector('#picture').content;
-
 const thumbnailsData = createDescriptionPhoto();
 
 const getThumbnail = ({ url, likes, comments }) => {
   const thumbnailElement = thumbnailsTemplate.cloneNode(true);
   const imageLink = thumbnailElement.querySelector('a.picture');
-  imageLink.href = url;
-
   const image = thumbnailElement.querySelector('img.picture__img');
-  image.src = url;
-
   const commentsContainer = thumbnailElement.querySelector('.picture__comments');
-  commentsContainer.textContent = comments.length;
-
   const likesContainer = thumbnailElement.querySelector('.picture__likes');
+
+  imageLink.href = url;
+  image.src = url;
+  commentsContainer.textContent = comments.length;
   likesContainer.textContent = likes;
 
-  const thumbnail = document.createDocumentFragment();
-  thumbnail.append(thumbnailElement);
-
-  return thumbnail;
+  return thumbnailElement;
 };
 
 const drawThumbnails = (pictData) => {
   const picturesList = document.createDocumentFragment();
 
   for (const pictDataElem of pictData) {
-
     const thumbnail = getThumbnail(pictDataElem);
-
     picturesList.append(thumbnail);
   }
 
   thumbnailsList.append(picturesList);
 };
 
-export { drawThumbnails };
+drawThumbnails(thumbnailsData);
+
